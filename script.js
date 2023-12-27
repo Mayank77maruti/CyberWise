0fdad4377e5eebaa7c83441e9ba01a3a0aa94ca9
Mayank77maruti <125661248+Mayank77maruti@users.noreply.github.com>
2023-12-27 14:02:22 +0530

    About Section Better

--- script.js
+++ script.js
@@ -16,3 +16,23 @@ $(document).ready(function(){
         $(this).next('.answer').toggleClass('show');
     });
 });
+
+function openGoogleDriveLink() {
+    window.open('https://drive.google.com/file/d/1zuEX5nU5BYEEQ4JoJa0yP537cRnojcxS/view?usp=sharing', '_blank');
+}
+function openfacebook() {
+    window.open('https://www.facebook.com/profile.php?id=100095593957170&mibextid=zLoPMf', '_blank');
+}
+function openinstagram() {
+    window.open('https://www.instagram.com/may_moh_77/', '_blank');
+}
+function openlinkedin() {
+    window.open('https://www.linkedin.com/in/mayank-mohapatra-697744267?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app', '_blank');
+}
+function opentwitter() {
+    window.open('https://twitter.com/Mayank77007?t=mJpicaGLL5kqxALIdCCXfg&s=09', '_blank');
+}
+function opengithub() {
+    window.open('https://github.com/Mayank77maruti', '_blank');
+}
+
